8491bf057dc7712e6cb4cf0ce82c636ffaf99f99
Martin Joiner <martin@martinjoiner.co.uk>
2018-10-30 17:14:48 +0000

    Improvements to hold a central master canvas on the server and update based on single pixel requests

--- README.md
+++ README.md
@@ -1,6 +1,31 @@
 
-To run the WebSockets server
+# Fructify
+
+A browser-based collaborative drawing game played over a network.
+
+![Screenshot](/docs/Screenshot-2018-10-30.png)
+
+## Installation
+
+- Clone repo down
+- Run `npm install`
+
+
+## Running the servers
+
+For this to work there needs to be 2 services running, a normal web server listening on http:// and a websocket server listening on ws://
+
+
+Run a normal web server over http:// with something like this...
+```
+php -S 192.168.2.99:8000
+```
+
+Run the WebSockets server with something like this...
 
 ```
-$ node index.js
+node index.js
 ```
+
+Obviously those IP addresses above will change dependant on your machine. 
+You may need to tweak the JavaScript that the browser uses to connect to the WebSocket.
--- index.js
+++ index.js
@@ -1,16 +1,45 @@
 
-var ws = require("nodejs-websocket")
+var ws = require("nodejs-websocket");
+var fs = require('fs');
+
+
+function padStringRight(string, length, char) {
+
+    if (string.length < length) {
+        for (var i = string.length; i < length; i++) {
+            string += char;
+        }
+    }
+
+    return string;
+}
+
 
 Service = function(){
 
+    var fileName = 'pixelString.txt';
+
+    var pixelsUntilNextSave = 30;
+
     this.connections = [];
-    this.message = '';
+
+    this.pixelCount = 3500;
+
+    // A 3500 character long string of zeros and ones that represent pixels on canvas (1 = coloured, 0 = transparent)
+    this.pixelString = padStringRight( fs.readFileSync(fileName).toString(), this.pixelCount, '0');
+
+
+
 
     this.addConnection = function( conn ){
         this.connections.push( conn );
         this.logConnections();
-        this.messageConnection( conn );
-    }
+
+        console.log('Sending canvas to new connection');
+        var json = this.getRenderCanvasMessage();
+        this.messageConnection( conn, json );
+    };
+
 
     this.removeConnection = function( conn ){
         var keyToDelete;
@@ -23,36 +52,86 @@ Service = function(){
         if( keyToDelete ){
             this.connections.splice( keyToDelete, 1);
         }
+
         this.logConnections();
-    }
+    };
+
 
     this.logConnections = function(){
         console.log("Connections now contains: " + this.connections.length + " items" );
-    }
+    };
 
-    this.messageAll = function( message ){
-        this.message = message;
-        for( var i in this.connections ){
-            this.messageConnection( this.connections[i] );
+
+    this.setPixel = function (index, value) {
+
+        console.log('Setting pixel ' + index + ' to ' + value);
+        this.pixelString = this.pixelString.substr(0, index) + value + this.pixelString.substr(index + 1);
+
+        if (pixelsUntilNextSave-- < 1) {
+            this.saveCanvas();
+            pixelsUntilNextSave = 30;
         }
-    }
 
-    this.messageConnection = function( conn ){
-        conn.sendText( this.message );
-    }
-}
+        this.broadcastCanvasToAllConnections();
+    };
+
+
+    this.getRenderCanvasMessage = function() {
+
+        var newData = {
+            action: 'render-canvas',
+            pixelString: this.pixelString,
+            connectionCount: this.connections.length
+        };
+
+        return JSON.stringify(newData);
+    };
+
+
+    this.broadcastCanvasToAllConnections = function() {
+
+        console.log("Sending canvas to all connections");
+        var json = this.getRenderCanvasMessage();
+
+        this.messageAllConnections(json);
+    };
+
+
+    this.messageAllConnections = function(message ){
+        for( var i in this.connections ){
+            this.messageConnection( this.connections[i], message );
+        }
+    };
+
+
+    this.messageConnection = function( conn, message ){
+        conn.sendText( message );
+    };
+
+
+    this.saveCanvas = function () {
+        fs.writeFile(fileName, this.pixelString, function() {
+            console.log("The file was saved!");
+        });
+    };
+
+};
 
 var service = new Service();
 
 var server = ws.createServer(function (conn) {
 
-    console.log("New connection")
+    console.log("New connection");
     service.addConnection( conn );
 
     conn.on("text", function (str) {
-        console.log("Received: \"" + str + "\"")
-        //conn.sendText(str.toUpperCase()+"!!!")
-        service.messageAll(str);
+
+        var data = JSON.parse(str);
+
+        if (data.action === 'set-pixel') {
+            service.setPixel(parseInt(data.index), data.value);
+
+        }
     });
 
     conn.on("close", function (code, reason) {
@@ -60,4 +139,9 @@ var server = ws.createServer(function (conn) {
         service.removeConnection( conn );
     });
 
-}).listen(8001);
+    conn.on("error", function (err) {
+        console.log("Caught flash policy server socket error: ");
+        console.log(err.stack);
+    });
+
+}).listen(8002);
